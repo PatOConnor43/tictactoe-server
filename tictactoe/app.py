1fece4e8ffc1a224870fe2e933901567245ca6aa
Pat O'Connor <patoconnor43@gmail.com>
2021-10-17 17:01:40 -0700

    Start game now returns json with an ID and also the game state

--- tictactoe/app.py
+++ tictactoe/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 import json
+import base64
+from enum import Enum
 
 app = Flask(__name__)
+
+class GameStates(Enum):
+    PLAYER_WINS = 'PLAYER_WINS'
+    COMPUTER_WINS = 'COMPUTER_WINS'
+    DRAW = 'DRAW'
+    PLAYING = 'PLAYING'
+
 
 @app.route("/")
 def hello_world():
@@ -9,4 +18,11 @@ def hello_world():
 
 @app.route("/start")
 def start_game():
-    return json.dumps({'game': 'on'})
+    game = {}
+    board = [[None, None, None], [None, None, None], [None, None, None]]
+    state = GameStates.PLAYING
+    game['board'] = board
+    game['state'] = state.name
+    json_game = json.dumps(game)
+    identifier = base64.b64encode(json_game.encode('ascii')).decode('UTF-8')
+    return json.dumps({'id': identifier, 'game': game})
