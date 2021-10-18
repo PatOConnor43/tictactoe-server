ce19226e495c63bf37f52c319a3ca69b2ced43c4
Pat O'Connor <patoconnor43@gmail.com>
2021-10-17 17:19:08 -0700

    Add 'str' as parent to enums so we can JSON serialize them

--- tictactoe/app.py
+++ tictactoe/app.py
@@ -5,12 +5,16 @@ from enum import Enum
 
 app = Flask(__name__)
 
-class GameStates(Enum):
+class GameStates(str, Enum):
     PLAYER_WINS = 'PLAYER_WINS'
     COMPUTER_WINS = 'COMPUTER_WINS'
     DRAW = 'DRAW'
     PLAYING = 'PLAYING'
 
+class SquareValues(str, Enum):
+    X = 'X'
+    O = 'O'
+    BLANK = ' '
 
 @app.route("/")
 def hello_world():
@@ -19,10 +23,14 @@ def hello_world():
 @app.route("/start")
 def start_game():
     game = {}
-    board = [[None, None, None], [None, None, None], [None, None, None]]
+    board = [
+        [SquareValues.BLANK, SquareValues.BLANK, SquareValues.BLANK],
+        [SquareValues.BLANK, SquareValues.BLANK, SquareValues.BLANK],
+        [SquareValues.BLANK, SquareValues.BLANK, SquareValues.BLANK]
+    ]
     state = GameStates.PLAYING
     game['board'] = board
-    game['state'] = state.name
+    game['state'] = state
     json_game = json.dumps(game)
     identifier = base64.b64encode(json_game.encode('ascii')).decode('UTF-8')
     return json.dumps({'id': identifier, 'game': game})
